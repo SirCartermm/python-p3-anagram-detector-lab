0776623623cad58303ac5530ca8974103e088bdb
SirCartermm <samuel.maina1@student.moringaschool.com>
2024-05-26 18:42:23 +0300

    updated anagram.py

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
-# your code goes here!
+# lib/anagram.py
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, words):
+        anagrams = []
+        for word in words:
+            if self.is_anagram(word):
+                anagrams.append(word)
+        return anagrams
+
+    def is_anagram(self, word):
+        return sorted(self.word) == sorted(word)
